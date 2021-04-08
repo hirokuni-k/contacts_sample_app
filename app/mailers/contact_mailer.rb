dff67f86ec2f40d24dd084bb78f68157cd529fc3
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-08 10:30:10 +0900

    お問い合わせ用のメイラーを設定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,19 @@
 class ContactMailer < ApplicationMailer
-  def user_email(name:, email:)
-    @name = name
-    mail(
-      to: email,
-      subject: "【アプリ名】お問い合わせを受付いたしました",
-    )
+  ADMIN_EMAIL = "hk.teka@gmail.com"
+
+  def user_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【アプリ名】お問い合わせを受付いたしました"
+
+    mail(to: contact.email, subject: subject)
   end
 
-  def admin_email
+  def admin_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【アプリ名】お問い合わせがありました"
+
+    mail(to: ADMIN_EMAIL, subject: subject)
   end
 end
